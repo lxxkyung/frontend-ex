3feb1b56c96750210a003aa3a95c2783fddc4df5
2kyunghee <lxxkyung@onflou.com>
2023-09-15 16:31:44 +0900

    Chore :: craco 수정

--- craco.config.js
+++ craco.config.js
@@ -1,17 +1,13 @@
-const path = require('path');
+const CracoAlias = require('craco-alias');
 
 module.exports = {
-    webpack: {
-        alias: {
-            '@': path.resolve(__dirname, 'src/'),
-            '@services': path.resolve(__dirname, 'src/services'),
-            '@hooks': path.resolve(__dirname, 'src/hooks'),
-            '@utils': path.resolve(__dirname, 'src/common/utils'),
-            '@components': path.resolve(__dirname, 'src/components'),
-            '@routes': path.resolve(__dirname, 'src/common/routes'),
-            '@pages': path.resolve(__dirname, 'src/pages'),
-            '@styled': path.resolve(__dirname, 'src/common/styled'),
-            '@state': path.resolve(__dirname, 'src/state'),
+    plugins: [
+        {
+            plugin: CracoAlias,
+            options: {
+                source: 'jsconfig',
+                tsConfigPath: 'jsconfig.paths.json',
+            },
         },
-    },
+    ],
 };
